8fffb4f0e23dab5763b1542e86f6031b1079c2dd
Midhath Ahmed <mdhthahmd@gmail.com>
2021-09-13 22:55:40 +0500

    devenv-setup script completed

--- scripts/devenv-setup.sh
+++ scripts/devenv-setup.sh
@@ -4,30 +4,62 @@
 # Licensed under the MIT License.
 #-------------------------------------------------------------------------------------------------------------
 #
-# Syntax: ./devenv-setup.sh [project name] [development environment]
-
-PROJECT=${1:-"new-project"}
-DEVENV=${2:-"dotnet-sdk-5.0.400-nodejs-14.17.6"}
-REMOTE_DEVENV=https://github.com/mdhthahmd/devcontainers.git
+# Syntax: ./devenv-setup.sh [project name]
 
 set -e
 
+PROJECT=${1:-"new-project"}
+
+GH_USER=mdhthahmd
+GH_REPO=devcontainers
+GH_BRANCH=main
+
+GH_API_URL=$(curl -s https://api.github.com/repos/$GH_USER/$GH_REPO/git/trees/$GH_BRANCH | jq -r '.tree[] | select(.path=="environments").url')
+GH_ENV_LIST=$( curl -s $GH_API_URL | jq -r '.tree[].path')
+
+declare -a DEV_ENVIRONMENTS=(`echo $GH_ENV_LIST | sed 's/\n/\n/g'`)
+noOfEnvs=${#DEV_ENVIRONMENTS[@]}
+
+while true; do
+    for (( i=0; i<${noOfEnvs}; i++ ));
+    do
+        echo "[$i] ${DEV_ENVIRONMENTS[$i]}"
+    done
+    echo ""; read -p "please choose an environment: `echo $'\n> '`" env
+
+    if [ "$env" -ge 0 ] && [ "$env" -le "$noOfEnvs" ]; then
+        break
+    fi
+done
+
+read -p "project name? ($PROJECT): " project
+
+# check if $PROJECT exists and prompt to overwrite
+if [ -d "$PROJECT" ]; then
+    while true; do
+        read -p "path $PROJECT already exists, overwrite? [Y/N]: " confirm
+        case $confirm in
+            [Yy]* ) rm -rf $PROJECT; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
 git init $PROJECT && cd $PROJECT
+
+REMOTE_DEVENV=https://github.com/$GH_USER/$GH_REPO.git
 
 git remote add origin $REMOTE_DEVENV
 git sparse-checkout init
-git sparse-checkout set "environments/$DEVENV"
+git sparse-checkout set "environments/${DEV_ENVIRONMENTS[env]}"
 
 git pull origin main
 
 # move all files and folders including dot prefixed ones
 shopt -s dotglob nullglob
-mv environments/$DEVENV/* .
+mv environments/${DEV_ENVIRONMENTS[env]}/* .
 shopt -u dotglob nullglob
 
 # clean up
 rm -rf environments .git
-
-
-# curl  https://api.github.com/repos/mdhthahmd/devcontainers/git/trees/main | jq -r '.tree[] | select(.path=="environments").url'
-# curl $(curl  https://api.github.com/repos/mdhthahmd/devcontainers/git/trees/main | jq -r '.tree[] | select(.path=="environments").url') | jq -r '.tree[].path'
